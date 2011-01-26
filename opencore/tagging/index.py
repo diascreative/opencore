d45fe8456d9d501e5a7bb67f273a79790f4553d3
junkafarian <fergus.doyle@largeblue.com>
2011-01-26 14:46:05 +0000

    (split) Tidying opencore.tagging

--- opencore/tagging/index.py
+++ opencore/tagging/index.py
@@ -1,31 +1,14 @@
-# Copyright (C) 2008-2009 Open Society Institute
-#               Thomas Moroz: tmoroz@sorosny.org
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License Version 2 as published
-# by the Free Software Foundation.  You may not use, modify or distribute
-# this program under any other version of the GNU General Public License.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
 import BTrees
 from zope.interface import implements
 from repoze.catalog.interfaces import ICatalogIndex
 
 
 class TagIndex(object):
-    """An index that defers to the tagging engine.
+    """ An index that defers to the tagging engine.
 
-    This index does not actually store anything.  It queries the
-    site tagging engine.  It relies on the fact that the catalog and
-    tagging engine use the same document map, so the docids match.
+        This index does not actually store anything.  It queries the
+        site tagging engine.  It relies on the fact that the catalog and
+        tagging engine use the same document map, so the docids match.
     """
     implements(ICatalogIndex)
     family = BTrees.family32
@@ -51,8 +34,9 @@ class TagIndex(object):
 
     def apply_intersect(self, query, docids):
         """ Run the query implied by query, and return query results
-        intersected with the ``docids`` set that is supplied.  If
-        ``docids`` is None, return the bare query results. """
+            intersected with the ``docids`` set that is supplied.  If
+            ``docids`` is None, return the bare query results.
+        """
         result = self.apply(query)
         if docids is None:
             return result
